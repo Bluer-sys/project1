e119a1808d6e639dd41e61427a7decacf8284310
Bluer <creeper234123@gmail.com>
2021-02-27 22:56:43 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// main.cpp: определяет точку входа для приложения.
+//
+
+#include "main.h"
+#include "lib.h"
+
+int main()
+{
+	std::cout << "build " << version() << "\n";
+	std::cout << "Hello, World!\n";
+	return 0;
+}
