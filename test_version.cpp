d7ce7b3b619fec6a807654c0169018a1cc1bdb52
Bluer <creeper234123@gmail.com>
2021-02-28 16:04:38 +0300

    11 commit

--- test_version.cpp
+++ test_version.cpp
@@ -1,10 +1,10 @@
-#define BOOST_TEST_MODULE helloworld_test_module
+#define BOOST_TEST_MODULE hello_world_test_module
 #include "lib.h"
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
+BOOST_AUTO_TEST_SUITE(hello_world_test_suite)
 
-BOOST_AUTO_TEST_CASE(helloworld_test_version)
+BOOST_AUTO_TEST_CASE(hello_world_test_version)
 {
     BOOST_CHECK(version() > 0);
 }
